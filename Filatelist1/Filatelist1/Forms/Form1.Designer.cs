2f02c6c67aa8fd1544aee808084c838b7f193143
locoyoda121 <locoyoda121@gmail.com>
2016-05-28 09:16:26 +0300

    Add files via upload

--- Filatelist1/Filatelist1/Forms/Form1.Designer.cs
+++ Filatelist1/Filatelist1/Forms/Form1.Designer.cs
@@ -213,7 +213,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(481, 325);
+            this.button1.Location = new System.Drawing.Point(481, 306);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 36);
             this.button1.TabIndex = 13;
